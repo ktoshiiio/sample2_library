8d5cd5dae56f4ba91f14315c2f2b0affc85c0745
toshio kaneishi <ktoshiiio@gmail.com>
2024-02-12 15:48:58 +0900

    追加.

--- MathLibrary/build.gradle.kts
+++ MathLibrary/build.gradle.kts
@@ -69,8 +69,8 @@ afterEvaluate{
         create("release", MavenPublication::class.java) {
             groupId = "co.jp.empolio"
             artifactId = "sample2_library"
-            version = "0.1.1"
-//            artifact("$buildDir/outputs/aar/Mathlibrary-release.aar")
+            version = "0.1.3"
+            artifact("$buildDir/outputs/aar/Mathlibrary-release.aar")
         }
     }
     }
